e55bfbbc33ec2cbb3fc96d79d414d9a82702ef15
orellanamilton <orellanamilton@hotmail.com>
2021-03-18 20:57:27 -0300

    Bootstrap para tabla

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./containers/Home/Home";
 import { Login } from "./containers/Login/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App = () => (
   <BrowserRouter>
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,29 +1,33 @@
 import '../styles/Table.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserEdit, faUserMinus  } from "@fortawesome/free-solid-svg-icons";
 
 export default function Table({users}) {
 
     return (
         <div className='container'>
-            <table className='table-container'>
-                <thead>
-                    <tr className='header'>
-                        <th>Nombre</th>
-                        <th>Correo electronico</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        users.length ? users.map(user => (
-                            <tr>
-                                <td>{user.name}</td>
-                                <td>{user.email}</td>
-                                <td>{user.username}</td>
-                            </tr>
-                        )) : <tr><td>Aún no hay usuarios registrados</td></tr>
-                    }
-                </tbody>
-            </table>
-        </div>
+                <table className='table table-dark table-striped'>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Correo electronico</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.length ? users.map(user => (
+                                <tr>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.username}</td>
+                                    <td><FontAwesomeIcon size="1x" icon={faUserMinus}></FontAwesomeIcon></td>
+                                    <td><FontAwesomeIcon size="1x" icon={faUserEdit}></FontAwesomeIcon></td>
+                                </tr>
+                            )) : <tr><td>Aún no hay usuarios registrados</td></tr>
+                        }
+                    </tbody>
+                </table>
+            </div>
     )
 }
