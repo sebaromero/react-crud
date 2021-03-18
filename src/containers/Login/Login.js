f05f4dc9e7cb104b81bef381fd4dc7078ce98fa3
NickyPeto <73256808+NickyPeto@users.noreply.github.com>
2021-03-18 18:13:38 -0300

    Algunos cambios, imports

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,5 +1,6 @@
 import "./Login.css";
 import { Formik, Field, Form } from "formik";
+import { useState , useEffect } from "react";
 
 const Login = () => {
   return (
@@ -11,12 +12,16 @@ const Login = () => {
         password: ''
       }}>
         <Form className="form">
-            <label className="form__label" htmlFor="email">email</label>
-            <Field className="email" id="email" type="email" name="email" placeholder="Write down your email adress" required/>
-            <label className="label" htmlFor="password">Password</label>
-            <Field className="password" id="password" type="password" name="password" placeholder="Write down your password" required/>
-          <button type="submit" onSubmit={()=>{}}>Log In</button>
-          </Form>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <Field className="email" id="email" type="email" name="email" placeholder="Write down your email adress" required/>
+            </div>
+            <div className="password">
+              <label className="label" htmlFor="password">Password</label>
+              <Field className="password" id="password" type="password" name="password" placeholder="Write down your password" required/>
+            </div>
+            <button className="submit" type="submit" onSubmit={()=>{}}>Log In</button>
+        </Form>
       </Formik>
     </div>
   );
