c8433975acd0e460ee431dfe8b793f14398e7b11
NickyPeto <73256808+NickyPeto@users.noreply.github.com>
2021-03-17 21:14:42 -0300

    Formulario y estilos

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,9 +1,23 @@
 import "./Login.css";
+import { Formik, Field, Form } from "formik";
 
 const Login = () => {
   return (
-    <div>
-    <h1>Login</h1>
+    <div className="container">
+      <h1>Login</h1>
+      <Formik 
+      initialValues = {{
+        email: '',
+        password: ''
+      }}>
+        <Form className="form">
+            <label className="form__label" htmlFor="email">email</label>
+            <Field className="email" id="email" type="email" name="email" placeholder="Write down your email adress" required/>
+            <label className="label" htmlFor="password">Password</label>
+            <Field className="password" id="password" type="password" name="password" placeholder="Write down your password" required/>
+          <button type="submit" onSubmit={()=>{}}>Log In</button>
+          </Form>
+      </Formik>
     </div>
   );
 };
