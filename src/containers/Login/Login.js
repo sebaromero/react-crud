f28b0ab65808247e220ec991983bedfa69e03046
Sebastian Romero <romeroseba95@gmail.com>
2021-03-25 18:43:51 -0300

    Usuario validado Formik

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -6,8 +6,6 @@ import { useHistory } from 'react-router-dom';
 const validation = values => {
   let errors = {};
   
-  
-
   if (!values.email) {
     errors.email = 'Por favor, ingrese email';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
